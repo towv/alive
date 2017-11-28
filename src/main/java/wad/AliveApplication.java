268bd56454d5df933dbbfacbdd54d79f6d05c45b
Tommi Viiala <twviiala@gmail.com>
2017-11-28 14:25:07 +0200

    testit korjattu, productionproperties lisatty

---
+++ src/main/java/wad/AliveApplication.java
@@ -0,0 +1,24 @@
+package wad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliveApplication.class, args);
+    }
+
+    public static String githubUrl() {
+        return "https://github.com/towv/alive"+"https://travis-ci.org/towv/alive"+"https://wepalive.herokuapp.com/";
+    }
+
+    public static String travisUrl() {
+        return "https://travis-ci.org/towv/alive";
+    }
+
+    public static String herokuUrl() {
+        return "https://wepalive.herokuapp.com/";
+    }
+}
