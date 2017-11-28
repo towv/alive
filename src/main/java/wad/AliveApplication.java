4f088bc1352d9df5680f832d0819fd144806110d
Tommi Viiala <twviiala@gmail.com>
2017-11-28 14:38:31 +0200

    mergemerge

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -11,22 +11,14 @@ public class AliveApplication {
     }
 
     public static String githubUrl() {
-        return "https://github.com/towv/alive"+"https://travis-ci.org/towv/alive"+"https://wepalive.herokuapp.com/";
+        return "https://github.com/towv/alive" + "https://travis-ci.org/towv/alive" + "https://wepalive.herokuapp.com/";
     }
 
     public static String travisUrl() {
-<<<<<<< HEAD
         return "https://travis-ci.org/towv/alive";
     }
 
     public static String herokuUrl() {
         return "https://wepalive.herokuapp.com/";
-=======
-        return "https://github.com/towv/alive";
-    }
-
-    public static String herokuUrl() {
-        return "https://github.com/towv/alive";
->>>>>>> c68f1e619b99b9cac073439795c9954bdce83cd9
     }
 }
